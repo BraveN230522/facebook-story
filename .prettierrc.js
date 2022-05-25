cbee1a1a102d0572fb6b4a0a86e35e7ac6b4ee5e
Brave N <woodyntd@gmail.com>
2022-05-25 11:15:01 +0700

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  jsxBracketSameLine: false,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  proseWrap: 'always',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen(props) {
+  console.log(props);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
